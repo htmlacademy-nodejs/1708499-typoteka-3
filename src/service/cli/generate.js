f1bef516bb1589e0a1c94be3c58c400eb0ba1398
max <maxstbn@gmail.com>
2021-08-13 00:36:42 +0300

    feat: 2.15. Личный проект: покоритель файлов

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -8,22 +8,21 @@ const {
   MAX_COUNT,
   LIMIT_COUNT_MESSAGE,
   FILE_NAME,
-  TITLES,
-  ANNOUNCES,
-  CATEGORIES,
   ExitCode
 } = require(`../constants`);
+const FILE_SENTENCES_PATH = `./data/sentences.txt`;
+const FILE_TITLES_PATH = `./data/titles.txt`;
+const FILE_CATEGORIES_PATH = `./data/categories.txt`;
 
-
-const generatePublications = (count) => (
-  Array(count).fill({}).map(() => ({
-    title: TITLES[getRandomInt(0, TITLES.length - 1)],
-    createdDate: getCreatedDate(),
-    announce: shuffle(ANNOUNCES).slice(0, 4).join(` `),
-    fullText: shuffle(ANNOUNCES).slice(0, getRandomInt(1, ANNOUNCES.length - 1)).join(` `),
-    category: shuffle(CATEGORIES).slice(0, getRandomInt(1, CATEGORIES.length - 1)),
-  }))
-);
+const readContent = async (filePath) => {
+  try {
+    const content = await fs.readFile(filePath, `utf8`);
+    return content.trim().split(`\n`);
+  } catch (err) {
+    console.error(chalk.red(err));
+    return [];
+  }
+};
 
 const getCountPublication = (args) => {
   const [count] = args;
@@ -40,7 +39,19 @@ module.exports = {
       process.exit(ExitCode.error);
     }
 
-    const content = JSON.stringify(generatePublications(countPublication));
+    const sentences = await readContent(FILE_SENTENCES_PATH);
+    const titles = await readContent(FILE_TITLES_PATH);
+    const categories = await readContent(FILE_CATEGORIES_PATH);
+
+    const publications = Array(countPublication).fill({}).map(() => ({
+      title: titles[getRandomInt(0, titles.length - 1)],
+      createdDate: getCreatedDate(),
+      announce: shuffle(sentences).slice(0, 4).join(` `),
+      fullText: shuffle(sentences).slice(0, getRandomInt(1, sentences.length - 1)).join(` `),
+      category: shuffle(categories).slice(0, getRandomInt(1, categories.length - 1)),
+    }))
+
+    const content = JSON.stringify(publications);
 
     try {
       await fs.writeFile(FILE_NAME, content);
